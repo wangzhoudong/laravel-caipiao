30ad95f2d0257f11368231b8a32fc5dcef90d0ee
dd <wangzhoudong@liweijia.com>
2018-04-24 18:13:48 +0800

    更新

--- service/app/Console/Kernel.php
+++ service/app/Console/Kernel.php
@@ -27,7 +27,7 @@ class Kernel extends ConsoleKernel
 
         $schedule->call(function () {
             dealSsqData::update();
-        }) ->dailyAt("23:00"));//每天23:00运行任务
+        }) ->dailyAt("23:00");//每天23:00运行任务
 
     }
 
