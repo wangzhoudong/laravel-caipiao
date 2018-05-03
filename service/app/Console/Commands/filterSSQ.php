8aca9d3bdeff971b6e8a5e2fe192d8ba970910ca
dd <wangzhoudong@liweijia.com>
2018-05-03 15:03:35 +0800

    更新

--- service/app/Console/Commands/filterSSQ.php
+++ service/app/Console/Commands/filterSSQ.php
@@ -38,34 +38,33 @@ class filterSSQ extends Command
      */
     public function handle()
     {
-        $arr = [1,2,2,3,1,2];
-        if($arr) {
-//                dd($this->getTypeNum($arr));
-        }
-        //
         $id = 0;
-        $row = 3;
+        $row = 100;
         $deleteId = [];
         while (true) {
-           $data =  SsqFullModel::select('id',"red1","red2","red3","red4","red5","red6")->where('id','>',$id)->take(300)->get();
-           if(!$data) {break;}
+           $data =  SsqFullModel::select('id',"red1","red2","red3","red4","red5","red6")->where('id','>',$id)->take($row)->get();
+
+           if($data->count()==0) {
+               break;
+           }
 
            foreach ($data as $val) {
                $id = $val->id;
                $arr = [$val->red1,$val->red2,$val->red3,$val->red4,$val->red5,$val->red6];
-               //过滤4位连号
-               if($this->getContinueNum($arr)>3) {
+               //过滤
+               if(
+                   ($this->getContinueNum($arr)>=4) ||
+                   ($this->getContinueNum([$val->red1,$val->red2,$val->red3,$val->red4])>=3) ||
+                   ($this->getContinueNum([$val->red3,$val->red4,$val->red5,$val->red6])>=3) ||
+                   ($this->getMaxNum($arr)<10) ||
+                   ($this->getTypeNum($arr)==1) ||
+                   ($this->getFilterType($arr))
+               ) {
                     $deleteId[] = $val->id;
                }
-               //最大值必须是
-               if($this->getMaxNum($arr)<10) {
-                   $deleteId[] = $val->id;
-               }
-               //如果数字都在10位数段位那么过滤
-               if($this->getTypeNum($arr)==1) {
-                   $deleteId[] = $val->id;
-               }
-               if(count($deleteId)>1000) {
+
+
+               if(count($deleteId)>500) {
                    SsqFullModel::whereIn('id',$deleteId)->delete();
                    $deleteId = [];
                }
@@ -74,6 +73,8 @@ class filterSSQ extends Command
            }
            echo $id . "\r\n";
         }
+        SsqFullModel::whereIn('id',$deleteId)->delete();
+
 
 
     }
@@ -129,5 +130,30 @@ class filterSSQ extends Command
             }
         }
         return $type1 + $type2 + $type3;
+    }
+
+    /**
+     * 5个数字都在10位数的号段
+     * @param $arr
+     */
+    public function getFilterType($arr) {
+        $type1 = 0;
+        $type2=0;
+        $type3=0;
+        foreach ($arr as $val) {
+            if($val<=10) {
+                $type1++;
+            }
+            if($val>10 and $val<=20) {
+                $type2++;
+            }
+            if($val>20) {
+                $type3++;
+            }
+        }
+        if($type1>=5 ||  $type2>=5) {
+            return true;
+        }
+        return false;
     }
 }
